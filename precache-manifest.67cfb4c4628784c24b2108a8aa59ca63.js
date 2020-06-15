f6483052e7473e1569ff14c041018f016087b6d3
Luciana Castro <lucianaycastro@gmail.com>
2020-06-15 17:24:41 -0300

    Updates

---
+++ precache-manifest.67cfb4c4628784c24b2108a8aa59ca63.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1a24948913377ce48da5ce1cdae6e28",
+    "url": "/hooks/index.html"
+  },
+  {
+    "revision": "8eecee97d6d4ff5e2284",
+    "url": "/hooks/static/css/main.ea275ae4.chunk.css"
+  },
+  {
+    "revision": "ed72adbb80027188b990",
+    "url": "/hooks/static/js/2.8a0b2ad5.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/hooks/static/js/2.8a0b2ad5.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8eecee97d6d4ff5e2284",
+    "url": "/hooks/static/js/main.c9864eb7.chunk.js"
+  },
+  {
+    "revision": "c3ec05ec6c3f35bfc8fb",
+    "url": "/hooks/static/js/runtime-main.aee52ea5.js"
+  }
+]);
